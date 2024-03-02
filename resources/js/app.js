e84f26e7c0bacee00a0a2836006a1b704a52f082
Dan Mattern <dan@danmattern.com>
2024-03-02 18:40:32 -0500

    try fixing codeql

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,103 +7,103 @@ import parsley from 'parsleyjs/dist/parsley.js'
 window.$ = jQuery
 
 window.parsley = parsley
-
-import.meta.glob([
-  '../images/**'
+// eslint-disable-next-line no-unused-vars
+const modules = import.meta.glob([
+    '../images/**'
 ])
 
 $(function () {
-  const $responseForm = $('#response-form')
-  $responseForm.on('submit', function (e) {
-    e.preventDefault()
-    $responseForm.parsley().validate()
-    if (!$responseForm.parsley().isValid()) {
-      return
-    }
-    $('#messages').empty()
-    const headers = {}
-    $('.headers-row').each(function () {
-      headers[$('.header-name', $(this)).val()] = $('.header-value', $(this)).val()
-    })
-    $.ajax({
-      method: 'POST',
-      url: $('#save').data('save'),
-      data: {
-        slug: $('#slug').val(),
-        http_status: $('#http_status').val(),
-        body: $('#body').val(),
-        _token: $('input[name=_token]').val(),
-        headers
-      },
-      success: function () {
-        $responseForm.trigger('reset')
-        $('.headers-row').remove()
-      },
-      error: function (response) {
-        $('#messages').append(
+    const $responseForm = $('#response-form')
+    $responseForm.on('submit', function (e) {
+        e.preventDefault()
+        $responseForm.parsley().validate()
+        if (!$responseForm.parsley().isValid()) {
+            return
+        }
+        $('#messages').empty()
+        const headers = {}
+        $('.headers-row').each(function () {
+            headers[$('.header-name', $(this)).val()] = $('.header-value', $(this)).val()
+        })
+        $.ajax({
+            method: 'POST',
+            url: $('#save').data('save'),
+            data: {
+                slug: $('#slug').val(),
+                http_status: $('#http_status').val(),
+                body: $('#body').val(),
+                _token: $('input[name=_token]').val(),
+                headers
+            },
+            success: function () {
+                $responseForm.trigger('reset')
+                $('.headers-row').remove()
+            },
+            error: function (response) {
+                $('#messages').append(
                     `<div class="alert alert-dismissable alert-danger my-2">
                         <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
                         ${response.responseJSON.message}
                     </div>`
-        )
-      }
+                )
+            }
+        })
     })
-  })
-  const $resultsTable = $('#results-table')
-  const $endpointsTable = $('#endpoints-table')
-  setInterval(function () {
-    $.get($resultsTable.data('source'))
-      .then(response => {
-        if (!response.length) {
-          return
-        }
-        $('tbody', $resultsTable).empty()
-        for (const i of response) {
-          $('tbody', $resultsTable).append(`<tr>
+    const $resultsTable = $('#results-table')
+    const $endpointsTable = $('#endpoints-table')
+    setInterval(function () {
+        $.get($resultsTable.data('source'))
+            .then(response => {
+                if (!response.length) {
+                    return
+                }
+                $('tbody', $resultsTable).empty()
+                for (const i of response) {
+                    $('tbody', $resultsTable).append(`<tr>
                         <th>${i.url}</th>
                         <th>${!i.content.length ? '<i>empty</i>' : i.content}</th>
                         <th><pre>${JSON.stringify(i.headers, null, 4)}</pre></th>
                     </tr>`)
-        }
-      })
-    $.get($endpointsTable.data('source'))
-      .then(response => {
-        if (!response.length) {
-          return
-        }
-        $('tbody', $endpointsTable).empty()
-        for (const i of response) {
-          $('tbody', $endpointsTable).append(`<tr>
+                }
+            })
+        $.get($endpointsTable.data('source'))
+            .then(response => {
+                if (!response.length) {
+                    return
+                }
+                $('tbody', $endpointsTable).empty()
+                for (const i of response) {
+                    $('tbody', $endpointsTable).append(`<tr>
                         <th>${i.url}<button class="copy-url btn btn-outline-warning px-2 ms-2" data-copy="${i.url}" type="button"><i class="bi-clipboard"></i></button></th>
                         <th>${i.http_status}</th>
                         <th><pre>${i.body}</pre></th>
                         <th><pre>${JSON.stringify(i.headers, null, 4)}</pre></th>
                     </tr>`)
-        }
-      })
-  }, 5000)
-  $(document).on('click', '.copy-url', function (e) {
-    e.preventDefault()
-    navigator.clipboard.writeText($(this).data('copy'))
-  })
-  $('#clear-data').on('click', function () {
-    $.get($(this).data('source'))
-      .then(() => {
-        $('tbody', $resultsTable).empty()
-          .append(`<tr class="active empty-state">
+                }
+            })
+    }, 5000)
+    $(document).on('click', '.copy-url', function (e) {
+        e.preventDefault()
+        navigator.clipboard.writeText($(this).data('copy'))
+    })
+    $('#clear-data').on('click', function () {
+        $.get($(this).data('source'))
+            .then(() => {
+                $('tbody', $resultsTable).empty()
+                    .append(`<tr class="active empty-state">
                         <td colspan="3" class="text-center">Nothing to see here</td>
                     </tr>`)
-        $('tbody', $endpointsTable).empty()
-          .append(`<tr class="active empty-state">
+                $('tbody', $endpointsTable).empty()
+                    .append(`<tr class="active empty-state">
                         <td colspan="3" class="text-center">Nothing to see here</td>
                     </tr>`)
-      })
-  })
-  $('#add-header').on('click', function (e) {
-    e.preventDefault()
-    const nameId = `header-name-${Date.now()}`
-    const valueId = `header-name-${Date.now()}`
-    $('#headers').append(`<div class="headers-row d-flex justify-content-between">
+            })
+    })
+    $('#add-header').on('click', function (e) {
+        e.preventDefault()
+        const nameId = `header-name-${Date.now()}`
+        const valueId = `header-name-${Date.now()}`
+        $('#headers').append(`<div class="headers-row d-flex justify-content-between">
             <div class="col-5 justify-content-center">
                 <label for="${nameId}">Header name</label>
                 <input id="${nameId}" class="header-name form-control" type="text"/>
@@ -113,5 +113,5 @@ $(function () {
                 <input id="${valueId}" class="header-value form-control" type="text"/>
             </div>
         </div>`)
-  })
+    })
 })
