8b05232abb9952f5719e92f72aabaa5e376548f6
Dan Mattern <dan@danmattern.com>
2024-03-02 19:40:09 -0500

    add tests

--- tests/Feature/CatchControllerTest.php
+++ tests/Feature/CatchControllerTest.php
@@ -4,15 +4,10 @@ namespace Tests\Feature;
 
 use App\Models\Request;
 use App\Models\Response;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CatchControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
     public function testCatches(): void
     {
         $response = Response::factory()->create();
